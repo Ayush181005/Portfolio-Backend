cc4144a4a5a9c67f31d4101d4eba16ad2ac22be2
Ayush <ayush.singh18@outlook.com>
2022-08-23 00:34:49 +0530

    Added TODO comment for adding img in model

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -9,6 +9,10 @@ const PortfolioSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    // img: {
+    //     type: String,
+    //     required: true
+    // },
     desc: String,
     type: String,
     slug: String,
