a23e56c9b381b4fbba2da5adac2472e1230b82d5
Ayush <ayush.singh18@outlook.com>
2022-10-04 10:31:48 +0530

    Fixed CORS error

--- index.js
+++ index.js
@@ -13,27 +13,8 @@ const app = express();
 // const host = process.env.HOST; // development
 const port = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.theayush.in');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.use(express.json()); // middleware for parsing application/json in req.body
-app.use(cors()); // middleware for allowing cross-origin requests
+app.use(cors({origin: 'https://www.theayush.in'})); // middleware for allowing cross-origin requests
 
 // Checking the server
 app.get('/', (req, res) => {
